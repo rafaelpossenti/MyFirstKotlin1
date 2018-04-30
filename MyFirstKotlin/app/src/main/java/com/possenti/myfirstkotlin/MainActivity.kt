798725d91b5ba23483006c9b6332246955ef3a00
Rafael Possenti <rafael_possenti@hotmail.com>
2018-04-30 18:44:57 -0100

    First commit

---
+++ MyFirstKotlin/app/src/main/java/com/possenti/myfirstkotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.possenti.myfirstkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    //test commit
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
